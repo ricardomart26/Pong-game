6de53dc4ee0ab50ac658c5efaf003d9eccfd1572
ricardomart26 <ricardomartins2626t@gmail.com>
2023-08-15 18:46:30 +0100

    Finished collisions and score of game

--- pong_frontend/src/components/Canvas.tsx
+++ pong_frontend/src/components/Canvas.tsx
@@ -1,36 +1,106 @@
 import { exit } from "process";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
+
+/**
+ * TODO:
+ * 
+ * - Meter o texto direito do score mais para a esquerda
+ * - Perceber o porque de encravar as pazinhas
+ * - Parte de baixo da pazinha não muda a direção da bola
+ * - Meter as pazinhas no meio depois do score
+ * 
+ */
 interface Object {
     x: number;
     y: number;
     color: string;
-    height?: number;
+    height: number;
+    name: string;
+    width: number;
+    score?: number;
 };
 
-
-const SPEED = 1.5;
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
+const SPEED = 2.2;
 
 const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     let ctx: CanvasRenderingContext2D;
     const listKeys: string[] = [];
-    let direction: number = 1;
-    const width = 500;
-    const height = 500;
-    const players: Object[] = [{x: 30, y: 250, color: 'yellow'}, {x: 450, y: 250, color: 'blue'}, {x: width / 2, y: height / 2, color: 'black', height: 10}]
+    let ball_direction_x: number = 1;
+    let ball_direction_y: number = 1;
+    // let curve: number = 0;
+    let curve: number = 0;
+    let idle: boolean = false;
+
+
+    const players: Object[] = [{
+        x: 30,
+        y: CANVAS_HEIGHT / 2,
+        color: 'yellow',
+        height: 70,
+        width: 10,
+        name: "player",
+        score: 0
+    },
+    {
+        x: CANVAS_WIDTH - 40,
+        y: CANVAS_HEIGHT / 2,
+        color: 'red',
+        height: 70,
+        width: 10,
+        name: "player2",
+        score: 0
+    },
+    {
+        x: CANVAS_WIDTH / 2,
+        y: CANVAS_HEIGHT / 2,
+        color: 'black',
+        height: 10,
+        width: 10,
+        name: "ball"
+    }];
+
+
+    const check_score = () => {
+        if (players[2].x > CANVAS_WIDTH)
+        {
+            if (players[0].score != undefined)
+                players[0].score++;
+            console.log("Score: " + players[0].score);
+            players[2].x = CANVAS_WIDTH / 2;
+            players[2].y = CANVAS_HEIGHT / 2;
+            curve = 0;
+            if (players[0].score === 11)
+                idle = true;
+        }
+        else if (players[2].x < 0)
+        {
+            if (players[1].score != undefined)
+                players[1].score++;
+            players[2].x = CANVAS_WIDTH / 2;
+            players[2].y = CANVAS_HEIGHT / 2;
+            curve = 0;
+            if (players[1].score === 11)
+                idle = true;
+
+        }
+    };
+
+    const colision = (player: number): boolean => {
+        return ((players[2].x >= players[player].x - 10 && players[2].x <= players[player].x + 10)
+            && (players[2].y >= players[player].y - 35 && players[2].y <= players[player].y + 35))
+    };
 
     const move = (move: number, player: number) => {
         players[player].y += move * SPEED;
     }
 
-    const colision = (player: number): boolean => {
-        return ((players[2].x >= players[player].x - 7 && players[2].x <= players[player].x + 7)
-            && (players[2].y >= players[player].y - 35 && players[2].y <= players[player].y + 35))
-    }
 
-    const update = () => {
+    const handle_moves = () => {
         if (listKeys.includes('w'))
             move(-1, 0);
         else if (listKeys.includes('s'))
@@ -39,38 +109,82 @@ const Canvas = () => {
             move(-1, 1);
         else if (listKeys.includes('ArrowDown'))
             move(1, 1);
-        
-        players[2].x += SPEED * direction;
-        
-        if (direction == 1 && colision(1))
-            direction = -1;
-        else if (direction == -1 && colision(0))
-            direction = 1;
+    };
 
-        if (players[2].x > width || players[2].x < 0)
+    const update = () => {
+
+        handle_moves();
+
+        players[2].x += SPEED * ball_direction_x;
+        if (curve != 0)
+            players[2].y += (1.0 + curve) * ball_direction_y;
+        // console.log("players y: ", players[2].y, " curve: ", curve);
+        // console.log("ball direction x: ", ball_direction_x, " : ", ball_direction_y);
+
+
+        if (ball_direction_x == 1 && colision(1))
         {
-            players[2].x = width / 2;
-            players[2].y = height / 2
+            curve = (players[2].y > players[1].y ? players[2].y - players[1].y : 0);
+            curve = (players[2].y < players[1].y ? players[1].y - players[2].y : 0);
+            while (curve > 10)
+                curve = (curve / 10);
+            // console.log("curve: ", curve);
+            ball_direction_x = -1;
         }
+        else if (ball_direction_x == -1 && colision(0))
+        {
+            curve = (players[2].y > players[0].y ? players[2].y - players[0].y : 0);
+            curve = (players[2].y < players[0].y ? players[0].y - players[2].y : 0);
+            while (curve > 10)
+                curve = (curve / 10);
+            ball_direction_x = 1;
+        }
+
+        // Touch the end line, should increment score;
+        check_score();
+
+        if (players[2].y >= CANVAS_HEIGHT - (players[2].height || 0))
+            ball_direction_y = -1;
+        else if (players[2].y < 0 + (players[2].height || 0))
+            ball_direction_y = 1;
+
 
     }
 
     const renderObj = () => {
 
-        update();
+        if (!idle)
+            update();
 
         ctx.fillStyle = 'green';
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        
-        players.forEach(player => { 
+        ctx.font = "30px Courier New";
+        ctx.fillStyle = "red";
+        ctx.fillText((players[0].score?.toString() || "0"), 30, 50);
+        ctx.fillText((players[1].score?.toString() || "0"), CANVAS_WIDTH - 40, 50);
+        players.forEach(player => {
             ctx.fillStyle = player.color;
-            ctx.fillRect(player.x, player.y - (player.height || 70) / 2, 10, player.height || 70);
+            if (player.y <= (player.height / 2))
+                player.y = (player.height / 2);
+            else if (player.y >= CANVAS_HEIGHT - (player.height / 2))
+                player.y = CANVAS_HEIGHT - (player.height / 2);
+            if (player.name === "ball")
+            {
+                ctx.beginPath();
+                ctx.fillStyle = player.color;
+                ctx.arc(player.x, player.y, player.height, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.closePath();
+            }
+            else
+                ctx.fillRect(player.x, player.y - player.height / 2, player.width, player.height);
         });
         requestAnimationFrame(renderObj);
 
     }
-    
+
     const init = () => {
+        ctx = canvasRef.current?.getContext("2d") as CanvasRenderingContext2D;
         window.addEventListener('keydown', (e: KeyboardEvent) => {
             if (!listKeys.includes(e.key))
                 listKeys.push(e.key);
@@ -79,7 +193,6 @@ const Canvas = () => {
         {
            listKeys.splice(listKeys.indexOf(e.key), 1);
         });
-        ctx = canvasRef.current?.getContext("2d") as CanvasRenderingContext2D;
         requestAnimationFrame(renderObj);
     }
 
@@ -88,7 +201,7 @@ const Canvas = () => {
     }, []);
 
     return (
-        <canvas ref={canvasRef} width={width} height={height}></canvas>
+        <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>
     );
 }
 
