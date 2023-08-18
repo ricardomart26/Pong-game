de09304ce67716947a22117fe4de34bf5cab00b1
ricardomart26 <ricardomartins2626t@gmail.com>
2023-08-18 18:41:58 +0100

    Added dialog for score to win, and the finish game

---
+++ pong_frontend/src/interfaces/Object.tsx
@@ -0,0 +1,11 @@
+
+export interface Object {
+    x: number;
+    y: number;
+    color: string;
+    height: number;
+    name: string;
+    width: number;
+    score?: number;
+    speed: number;
+};
