5ceb8813771f90c0d2a7307c8f276e5274822684
ricardomart26 <ricardomartins2626t@gmail.com>
2023-08-18 17:18:47 +0100

    Added profile component

---
+++ pong_frontend/src/interfaces/Player.tsx
@@ -0,0 +1,7 @@
+
+
+export interface Player {
+    name: string;
+    email: string;
+    avatarUrl: string;
+}
--- pong_frontend/src/scenes/game/index.tsx
+++ pong_frontend/src/scenes/game/index.tsx
@@ -1,14 +1,30 @@
 import Canvas from "../../components/Canvas";
 import { Box, Icon } from "@mui/material";
 import { KeyboardArrowUp, KeyboardArrowDown } from "@mui/icons-material";
+import Profile from "../../components/Profile";
+import { Player } from "../../interfaces/Player";
+// import playerAvatar1 from ;
+// import playerAvatar2 from ; 
+
 
 const Game = () => {
 
+    const player1: Player = {
+        name: "Player1",
+        email: "Player1@gmail.com",
+        avatarUrl: process.env.PUBLIC_URL + '/Player1.png',
+    }
+
+    const player2: Player = {
+        name: "Player2",
+        email: "Player2@gmail.com",
+        avatarUrl: process.env.PUBLIC_URL + '/Player2.png',
+    }
+
+
     let colorTransparencyStyle = "0.2";
 
-
     const changeKeysBackgound = (activate: boolean) => {
-        
         colorTransparencyStyle = activate ? "0.5" : "0.2";
     }
 
@@ -16,14 +32,20 @@ const Game = () => {
         <div>
             <h1>Pong game</h1>
             <Box display="flex" justifyContent="center">
+                <Box m="50px">
+                    <Profile player={player1}/>
+                </Box>
                 <Canvas changeKeys={changeKeysBackgound}/>
-                <Box display="flex" justifyContent="center" flexDirection="column">
+                {/* <Box display="flex" justifyContent="center" flexDirection="column">
                     <Box>
                         <KeyboardArrowUp/>
                     </Box>
                     <Box sx={{backgroundColor: `rgba(0, 0, 0, ${colorTransparencyStyle}))`}}>
                         <KeyboardArrowDown/>
                     </Box>
+                </Box> */}
+                <Box m="50px">
+                    <Profile player={player2}/>
                 </Box>
             </Box>
         </div>
